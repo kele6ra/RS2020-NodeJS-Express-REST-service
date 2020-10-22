34cf0ba8065ae2b8edc842e774b80c8e2056505a
Andrei Auchynnikau <kele6ra@gmail.com>
2020-10-22 23:16:57 +0300

    fix: delete manual validation

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,16 +1,9 @@
 const Board = require('./board.model');
 const boardsRepo = require('./board.mongo.repository');
 const tasksService = require('../tasks/task.service');
-const BAD_REQUEST_ERROR = require('../../errors/400');
 
 const addBoard = boardData => {
   const board = new Board(boardData);
-  if (
-    typeof board.title === 'undefined' ||
-    typeof board.columns === 'undefined'
-  ) {
-    throw new BAD_REQUEST_ERROR('Board title or columns are not set!');
-  }
   return boardsRepo.addBoard(board.get());
 };
 
@@ -19,24 +12,14 @@ const deleteBoard = boardId => {
   tasksService.deleteBoardTasks(boardId);
 };
 
-const getAll = () => {
-  return boardsRepo.getAll();
-};
+const getAll = () => boardsRepo.getAll();
 
-const getBoard = boardId => {
-  return boardsRepo.getBoard(boardId);
-};
+const getBoard = boardId => boardsRepo.getBoard(boardId);
 
 const updateBoard = (boardId, boardData) => {
   const board = boardsRepo.getBoard(boardId);
   const editedBoard = new Board(board);
   editedBoard.set(boardData);
-  if (
-    typeof editedBoard.title === 'undefined' ||
-    typeof editedBoard.columns === 'undefined'
-  ) {
-    throw new BAD_REQUEST_ERROR('Board title or columns are not set!');
-  }
   return boardsRepo.updateBoard(editedBoard.get());
 };
 
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,13 +1,9 @@
 const tasksService = require('../tasks/task.service');
 const usersRepo = require('./user.mongo.repository');
 const User = require('./user.model');
-const BAD_REQUEST_ERROR = require('../../errors/400');
 
 const addUser = userData => {
   const user = new User(userData);
-  if (typeof user.name === 'undefined') {
-    throw new BAD_REQUEST_ERROR('User name is not set!');
-  }
   return usersRepo.addUser(user.get());
 };
 
@@ -26,9 +22,6 @@ const updateUser = (userData, userId) => {
   const user = usersRepo.getUser(userId);
   const editedUser = new User(user);
   editedUser.set(userData);
-  if (typeof user.name === 'undefined') {
-    throw new BAD_REQUEST_ERROR('User name is not set!');
-  }
   return usersRepo.updateUser(editedUser.get());
 };
 
